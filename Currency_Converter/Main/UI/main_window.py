7605739ad8eda53135789fd42451c319ad1b5f45
Blood <BloodCZE@gmail.com>
2024-02-05 21:09:46 +0100

    - changed API - takes different input variations

--- Currency_Converter/Main/UI/main_window.py
+++ Currency_Converter/Main/UI/main_window.py
@@ -32,6 +32,7 @@ class Ui_mw_Main(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.groupBox_3 = QGroupBox(mw_Main)
         self.groupBox_3.setObjectName(u"groupBox_3")
+        self.groupBox_3.setAutoFillBackground(False)
         self.groupBox_3.setFlat(False)
         self.groupBox_3.setCheckable(False)
         self.gridLayout_2 = QGridLayout(self.groupBox_3)
@@ -61,10 +62,18 @@ class Ui_mw_Main(object):
 
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_2)
 
-        self.le_InputAmount = QLineEdit(self.groupBox)
-        self.le_InputAmount.setObjectName(u"le_InputAmount")
+        self.le_InputValue = QLineEdit(self.groupBox)
+        self.le_InputValue.setObjectName(u"le_InputValue")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.le_InputValue.sizePolicy().hasHeightForWidth())
+        self.le_InputValue.setSizePolicy(sizePolicy)
+        self.le_InputValue.setFocusPolicy(Qt.StrongFocus)
+        self.le_InputValue.setFrame(True)
+        self.le_InputValue.setClearButtonEnabled(False)
 
-        self.formLayout.setWidget(3, QFormLayout.SpanningRole, self.le_InputAmount)
+        self.formLayout.setWidget(3, QFormLayout.SpanningRole, self.le_InputValue)
 
 
         self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 1)
@@ -85,10 +94,11 @@ class Ui_mw_Main(object):
 
         self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label_4)
 
-        self.le_OutputAmount = QLineEdit(self.groupBox_2)
-        self.le_OutputAmount.setObjectName(u"le_OutputAmount")
+        self.le_OutputValue = QLineEdit(self.groupBox_2)
+        self.le_OutputValue.setObjectName(u"le_OutputValue")
+        self.le_OutputValue.setReadOnly(True)
 
-        self.formLayout_2.setWidget(3, QFormLayout.SpanningRole, self.le_OutputAmount)
+        self.formLayout_2.setWidget(3, QFormLayout.SpanningRole, self.le_OutputValue)
 
         self.cb_OutputCurrency = QComboBox(self.groupBox_2)
         self.cb_OutputCurrency.addItem("")
@@ -107,6 +117,7 @@ class Ui_mw_Main(object):
 
         self.pb_Convert = QPushButton(self.groupBox_3)
         self.pb_Convert.setObjectName(u"pb_Convert")
+        self.pb_Convert.setAutoDefault(True)
 
         self.gridLayout_2.addWidget(self.pb_Convert, 2, 0, 1, 2)
 
@@ -120,14 +131,22 @@ class Ui_mw_Main(object):
 
         self.gridLayout.addWidget(self.l_Message, 2, 0, 1, 1)
 
+        QWidget.setTabOrder(self.le_InputValue, self.cb_InputCurrency)
+        QWidget.setTabOrder(self.cb_InputCurrency, self.cb_OutputCurrency)
+        QWidget.setTabOrder(self.cb_OutputCurrency, self.pb_Convert)
+        QWidget.setTabOrder(self.pb_Convert, self.le_OutputValue)
 
         self.retranslateUi(mw_Main)
+
+        self.cb_OutputCurrency.setCurrentIndex(1)
+        self.pb_Convert.setDefault(False)
+
 
         QMetaObject.connectSlotsByName(mw_Main)
     # setupUi
 
     def retranslateUi(self, mw_Main):
-        mw_Main.setWindowTitle(QCoreApplication.translate("mw_Main", u"Form", None))
+        mw_Main.setWindowTitle(QCoreApplication.translate("mw_Main", u"Currency Convertor", None))
         self.groupBox.setTitle(QCoreApplication.translate("mw_Main", u"Input", None))
         self.label.setText(QCoreApplication.translate("mw_Main", u"Currency", None))
         self.cb_InputCurrency.setItemText(0, QCoreApplication.translate("mw_Main", u"CZK", None))
@@ -135,13 +154,17 @@ class Ui_mw_Main(object):
         self.cb_InputCurrency.setItemText(2, QCoreApplication.translate("mw_Main", u"USD", None))
 
         self.label_2.setText(QCoreApplication.translate("mw_Main", u"Amount", None))
+        self.le_InputValue.setInputMask("")
+        self.le_InputValue.setText("")
+        self.le_InputValue.setPlaceholderText("")
         self.groupBox_2.setTitle(QCoreApplication.translate("mw_Main", u"Output", None))
         self.label_3.setText(QCoreApplication.translate("mw_Main", u"Currency", None))
         self.label_4.setText(QCoreApplication.translate("mw_Main", u"Amount", None))
-        self.cb_OutputCurrency.setItemText(0, QCoreApplication.translate("mw_Main", u"EUR", None))
-        self.cb_OutputCurrency.setItemText(1, QCoreApplication.translate("mw_Main", u"CZK", None))
+        self.cb_OutputCurrency.setItemText(0, QCoreApplication.translate("mw_Main", u"CZK", None))
+        self.cb_OutputCurrency.setItemText(1, QCoreApplication.translate("mw_Main", u"EUR", None))
         self.cb_OutputCurrency.setItemText(2, QCoreApplication.translate("mw_Main", u"USD", None))
 
+        self.cb_OutputCurrency.setCurrentText(QCoreApplication.translate("mw_Main", u"EUR", None))
         self.pb_Convert.setText(QCoreApplication.translate("mw_Main", u"Convert", None))
         self.l_Message.setText(QCoreApplication.translate("mw_Main", u"Message", None))
     # retranslateUi
--- Currency_Converter/Main/main.py
+++ Currency_Converter/Main/main.py
@@ -14,25 +14,83 @@ class MainWindow(qtw.QWidget, Ui_mw_Main):
 
         self.pb_Convert.clicked.connect(self.convert_currency)
 
+        self.le_InputValue.setFocus()
+
+        
+    def input_value_mod(self):
+        
+        InputValue = self.le_InputValue.text().upper()
+        InputValue = InputValue.replace("KČ", "CZK").replace("€", "EUR").replace("EURO", "EUR").replace("$", "USD")
+
+        return InputValue
+
+    
+    def get_input_amount(self):
+
+        InputValue = self.input_value_mod()
+
+        if self.is_currency_in_input():  # Remove the unnecessary self argument here (??? chat GPT, what???)
+            InputValue = InputValue.replace(self.get_input_currency(), "")    # remove currency from it
+
+        if InputValue.count("."):  # format US currency notation - remove ","
+            InputValue = InputValue.replace(",", "")
+            
+        if "," in InputValue:
+            if len(InputValue[InputValue.index(",")+1:]) >= 3 and InputValue[InputValue.index(",")+1:InputValue.index(",")+4].isdigit():        # 1,000 to 1000
+                InputValue = InputValue.replace(",", "")
+
+        InputValue = InputValue.replace(",", ".")  # format CZK(etc) currency notation - replace "," with "."
+        InputValue = InputValue.replace(" ", "")  # remove spaces
+
+        return InputValue
+    
+
+    def is_currency_in_input(self):
+
+        InputValue = self.le_InputValue.text().upper().replace(".", "").replace(",", "")
+        return not InputValue.isdigit()
+    
+
+    def get_input_currency(self):
+
+        currencies = ["EUR", "USD", "CZK"]
+
+        InputValue = self.input_value_mod()
+
+        for curr in currencies:
+            if InputValue.find(curr) >= 0:
+                self.cb_InputCurrency.setCurrentText(curr)
+                return curr  # Return the found currency (for the get_input_amount)
+
+        return ""  # Return an empty string if no currency is found (I don't understand why that's necessary)
+
         
     def convert_currency(self):
 
-        url = f"https://api.apilayer.com/exchangerates_data/convert?to={self.cb_OutputCurrency.currentText()}&from={self.cb_InputCurrency.currentText()}&amount={self.le_InputAmount.text()}"
+        if self.is_currency_in_input():
+            self.get_input_currency()
+
+        url = f"https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_DFWRlBKAKDksrqpckRt7d1wCqtmX3cJy4nkng6xg&currencies={self.cb_OutputCurrency.currentText()}&base_currency={self.cb_InputCurrency.currentText()}"
 
         payload = {}
         headers= {
-        "apikey": "TVeSAhOa6ota8Hv0rUuU1jCPTNkEaFdV"
+        "apikey": "fca_live_DFWRlBKAKDksrqpckRt7d1wCqtmX3cJy4nkng6xg"
         }
 
         response = requests.request("GET", url, headers=headers, data = payload)
 
+        print(response)
+
         status_code = response.status_code
         result = response.json()
 
-        output = round(result["result"], 2)
+        output = round((result["data"][self.cb_OutputCurrency.currentText()]*float(self.get_input_amount())), 2)     #round[OutputCurr x InputAmount), 2]
+        output = str(output) + " " + self.cb_OutputCurrency.currentText()       #just formating
+        self.le_OutputValue.setText(str(output))
 
-        self.le_OutputAmount.setText(str(output))
 
+        self.l_Message.setNum(status_code)      #Error code
+        
 
 
 
